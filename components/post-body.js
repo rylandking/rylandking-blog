a34b3dd31f69d535004d56a59cab735d6f3e9fb5
rylandking <ryland@users.noreply.github.com>
2020-10-01 11:40:34 -0600

    fix image 7

--- components/post-body.js
+++ components/post-body.js
@@ -28,7 +28,7 @@ const serializers = {
 		figure: ({ node }) => {
 			const { image, alt } = node;
 			return (
-				<div className='flex justify-center w-full h-18rem'>
+				<div className='flex justify-center w-full h-64 md:h-auto'>
 					<img
 						src={urlFor(image.asset).url()}
 						alt={`${alt}`}
